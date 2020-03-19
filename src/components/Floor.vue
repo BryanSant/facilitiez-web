c75c46fb8b6967371ff98facf1522b3ce33423f8
Bryan Sant <bryan.sant@gmail.com>
2020-03-19 15:50:22 -0600

    Visual fixes

--- src/components/Floor.vue
+++ src/components/Floor.vue
@@ -6,18 +6,24 @@
             <v-card class="pa-1 ma-1 elevation-2" color="#ddd" width="8em" height="8em">
                 <span class="caption text-no-wrap" style="background: #bbb; display: block; text-align: center">S5.S027</span>
                 <br/>
-                <v-avatar color="indigo" size="32" class="mr-1">
-                    <v-icon dark>mdi-account-circle</v-icon>
-                </v-avatar>
-                <span class="caption text-no-wrap" style="display: inline-block">Bryan Sant</span>
+                <div style="text-align: center">
+                    <v-avatar color="orange" size="36" class="mr-1">
+                        <v-icon dark>mdi-account-circle</v-icon>
+                    </v-avatar>
+                    <br/>
+                    <span class="caption" style="display: inline-block">Bryan Sant</span>
+                </div>
             </v-card>
             <v-card class="pa-1 ma-1 elevation-2" color="#ddd" width="8em" height="8em">
                 <span class="caption text-no-wrap" style="background: #bbb; display: block; text-align: center">S5.S027</span>
                 <br/>
-                <v-avatar color="indigo" size="32" class="mr-1">
-                    <v-icon dark>mdi-account-circle</v-icon>
-                </v-avatar>
-                <span class="caption text-no-wrap" style="display: inline-block">Bryan Sant</span>
+                <div style="text-align: center">
+                    <v-avatar color="orange" size="36" class="mr-1">
+                        <v-icon dark>mdi-account-circle</v-icon>
+                    </v-avatar>
+                    <br/>
+                    <span class="caption" style="display: inline-block">Derek Clifford</span>
+                </div>
             </v-card>
         </v-row>
     </v-col>
